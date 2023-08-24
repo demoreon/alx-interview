9b635dd1202d5674a2b12fc206c5ef58e90a2b64
demoreon <demoreon@gmail.com>
2023-08-24 21:19:39 +0100

    make file

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Function that returns min num
+"""
+
+
+def makeChange(coins, total):
+    """
+    calculate and return need coins
+    Args:
+        coins list of coins of different values
+        total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
+    """
+    if total <= 0:
+        return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
+
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
